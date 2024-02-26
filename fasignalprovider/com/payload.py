f7428b643c1eee8ad1b6dfe3bc1d2d9a2a96edfa
svabra <brayan@brayan.com>
2024-02-26 07:00:08 +0100

    added serialization for time.

--- fasignalprovider/com/payload.py
+++ fasignalprovider/com/payload.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import Any, Dict, TypeVar, Optional
 from fasignalprovider.com.code import Code
 
@@ -18,6 +18,8 @@ class Event(Payload):
     event_type: str
     detail: Optional[str] = None
     code: Optional[Code]
+    data: Optional[Dict[str, Any]] = None  # with type hints
+    """Data is used to store the object value. e.g. a signal, a trade, a profit, etc."""
 
     def serialize(self) -> Dict[str, Any]:
         data = super().serialize()
@@ -26,9 +28,27 @@ class Event(Payload):
                 "event_type": self.event_type,
                 "detail": self.detail,
                 "code": self.code.value if self.code else None,
+                "data": self._serialize_data(self.data)
             }
         )
+        # Optionally, you can remove keys with None values if you don't want them in the serialized output
+        #return {k: v for k, v in data.items() if v is not None}
         return data
+    
+    def _serialize_data(self, data: Optional[Dict[str, Any]]) -> Optional[Dict[str, Any]]:
+        if data is None:
+            return None
+        # Serialize each value in the dictionary, converting datetime to ISO format if necessary
+        return {key: self._serialize_value(value) for key, value in data.items()}
+
+    def _serialize_value(self, value: Any) -> Any:
+        if isinstance(value, datetime):
+            # Convert datetime to ISO format string
+            return value.astimezone().isoformat()
+        elif isinstance(value, dict):
+            # Recursively serialize dictionary values
+            return self._serialize_data(value)
+        return value
 
 
 ### TECHNICAL EVENTS FROM HERE DOWNWARDS ###
@@ -41,17 +61,15 @@ class ErrorEvent(Event):
 
 ### BUSINESS EVENTS FROM HERE DOWNWARDS ###
     
-class SignalSubmitted(Event):
-    """This event occures when a trading signal has been submitted to us, but has not yet been completely received and stored."""
-    internal_signal_id: str
-    event_type: str = "signal_submitted"
-
+class SignalIncoming(Event):
+    """This event occures when a trading signal is incoming, but has not yet been completely received and stored."""        
+    signal_data: Dict
+    event_type: str = "signal_incoming"
 
 class SignalReceived(Event):
-    """This is when a trading signal was received without errors."""
+    """This is when a trading signal was received without errors."""    
     internal_signal_id: str
     event_type: str = "signal_received"
-
 
 class SignalRejected(Event):
     """This is when a trading signal was deliberately rejected after the qualification process has been completed."""
@@ -59,12 +77,17 @@ class SignalRejected(Event):
     reason: str
     event_type: str = "signal_rejected"
 
-
 class SignalQualified(Event):
-    """This event appears if a trading signals has successfully qualified."""
+    """This event appears if a trading signal has successfully qualified."""
     signal_id: str
-    time_of_qualification: datetime
+    time_of_qualification: datetime = Field(default_factory=lambda: datetime.utcnow())
     event_type: str = "signal_qualified"
+
+    def __init__(self, **data):
+        if 'time_of_qualification' in data:
+            # Ensure the provided datetime is in UTC
+            data['time_of_qualification'] = data['time_of_qualification'].astimezone(tz=timezone.utc)
+        super().__init__(**data)
 
 class TradeCreated(Event):
     """This event appears if a trade base on a signal was started. With us a trade consists of at least one buy and one sell signal (or TP/SL)."""
